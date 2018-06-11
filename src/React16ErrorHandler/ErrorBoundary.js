a9e35101050d942bb36cd32fa2960967e8703065
Junting <342766475@qq.com>
2018-06-11 13:24:06 +0800

    React16新特性-更友好的错误提示和错误处理

---
+++ src/React16ErrorHandler/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch (error, info) {
+        // 显示错误 UI
+        this.setState({ hasError: true });
+
+        // 同时输出错误日志
+        console.log(error, info)
+    }
+
+    render () {
+        if (this.state.hasError) {
+            return (
+                <h1>Oops, something went wrong.</h1>
+            )
+        }
+        return this.props.children;
+
+    }
+}
+
+export default ErrorBoundary
