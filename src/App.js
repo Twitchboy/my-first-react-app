97cad299061e77916f76e523da025f31625202cd
Junting <342766475@qq.com>
2018-06-21 10:09:26 +0800

    工具函数补充

--- src/App.js
+++ src/App.js
@@ -1,66 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import logo from './logo.svg';
+import Home from './components/Home';
+import Login from './components/Login';
 import './App.css';
-import ListComponent from './React16Render/ListComponent';
-import StringComponent from './React16Render/StringComponent';
-import ErrorBoundary from './React16ErrorHandler/ErrorBoundary';
-import { Profile } from './React16ErrorHandler/Profile';
-import Modal from './React16PortalsApi/Modal'
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: {name: 'Junting'},
-      showModal: true
-    }
-    this.handleError = this.handleError.bind(this)
-    this.closeModal = this.closeModal.bind(this)
-  }
-
-  componentDidMount() {
-    console.log
-  }
-
-  handleError (event) {
-    this.setState({
-      user: null
-    })
-  }
-
-  closeModal (event) {
-    this.setState({
-      showModal: false
-    })
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello world！
-        </p>
-        <ul>
-          <ListComponent />
-        </ul>
-        <p><StringComponent /></p>
-
-        <ErrorBoundary>
-          <Profile user={this.state.user} />
-        </ErrorBoundary>
-        <button onClick={this.handleError}>点击</button>
-
-        {
-          this.state.showModal && (
-            <Modal onClose={this.closeModal}>
-              Modal Dialog~
-            </Modal>
-          )
-        }
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/posts" component={Home}/>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/utils/date.js
@@ -0,0 +1,16 @@
+export const getFormatDat = (utc) =>  {
+    const date = new Date(utc);
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1 + "";
+    month = month.length === 1 ? "0" + month : month;
+    let day = date.getDate() + "";
+    day = day.length === 1 ? "0" + day : day;
+    let hour = date.getHours() + "";
+    hour = hour.length === 1 ? "0" + hour : hour;
+    let minute = date.getMinutes() + "";
+    minute = minute.length === 1 ? "0" + minute : minute;
+    let second = date.getSeconds() + "";
+    second = date.length === 1 ? "0" + second : second;
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`
+};
